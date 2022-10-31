afba08b19f3698055b1fbcc158217b720844fd4c
Roman <hgpdfllh@gmail.com>
2022-10-31 11:55:11 +0200

    Create first mini program

--- js/script.js
+++ js/script.js
@@ -1,103 +1,19 @@
-// "use strict";
+const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');;
 
-// let number = 5;
-// const leftBorderWidth = 1;
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// number = 10;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько его оцените?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько его оцените?', '');
 
-// console.log(number);
+personalMovieDB.movies[a] = b; // Как добавлять переменные в объект: вызываем у переменной (через точку) свойство и к этому свойству добавляем значение возвращаемое другой переменной (через квадратные скобки), потом присваиваем этой переменной другую переменную (её возвращаемое значение); 
+personalMovieDB.movies[c] = d;
 
-// const obj = {
-//     a: 50
-// };
-
-// obj.a = 10;
-
-// console.log(obj);
-
-
-// {
-//     let result = 50;
-// }
-
-// console.log(result);
-
-// alert(5);
-// [].push('a');
-
-
-// let number = 4.6;
-
-// console.log(-4 / 0);
-// console.log('string' * 9);
-
-// const person = '5';
-
-// const bool = false;
-
-// console.log(something);
-
-// let und;
-// console.log(und);
-
-// const obj = {
-//       firstName: 'John',
-//       age: 25,
-//       isMarried: false
-// };
-
-// console.log(obj.firstName);
-// console.log(obj['age']);
-
-// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-// console.log(arr[1]);
-
-
-// alert('Hello');
-
-// const result = confirm('Are you here?');
-// console.log(result);
-
-// const answer = +prompt('Вам есть 18?', '18');
-// console.log(answer + 5);
-
-// const answers = [];
-
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
-
-// console.log(typeof(answers));
-// console.log(typeof(null));
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user = 'Ivan';
-
-// alert(`Привет, ${user}`);
-
-// console.log('arr' + " - object");
-// console.log(4 + +"5");
-
-// let incr = 10,
-//     decr = 10;
-
-// ++incr;
-// --decr;
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5 % 2);
-
-// console.log(2 + 2 * 2 !== '6');
-
-// const isChecked = true,
-//     isClose = false;
-
-// console.log(isChecked || !isClose);
-
-
-
+console.log(personalMovieDB);
