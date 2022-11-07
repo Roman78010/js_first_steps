964375ab45d93182614c6ff608b03e866aad1d6c
Roman <hgpdfllh@gmail.com>
2022-11-07 11:31:47 +0200

    Passed lesson function (practice)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numbersOfFilms;
+
+function start() {
+    numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)) {
+        numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+// start();
 
 const personalMovieDB = {
     count: numbersOfFilms,
@@ -8,27 +18,56 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько его оцените?', '');
-            
-    if (a !== '' && b !== '' && a !== null && b !== null && a.length < 50 && b.length < 50) { // Если пользователь нажал "отменить": нам возвращается значение null;
-        personalMovieDB.movies[a] = b; // Как добавлять переменные в объект: вызываем у переменной (через точку) свойство и к этому свойству добавляем значение возвращаемое другой переменной (через квадратные скобки), потом присваиваем этой переменной другую переменную (её возвращаемое значение);
-        console.log('Done!');
-    } else {
-        console.log('Error!');
-        i--; // Если инструкция if, выше, не выполнелась, мы обратимся к ней снова (внутри цикла) с помощью этого выражения;
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько его оцените?', '');
+                
+        if (a !== '' && b !== '' && a !== null && b !== null && a.length < 50 && b.length < 50) { // Если пользователь нажал "отменить": нам возвращается значение null;
+            personalMovieDB.movies[a] = b; // Как добавлять переменные в объект: вызываем у переменной (через точку) свойство и к этому свойству добавляем значение возвращаемое другой переменной (через квадратные скобки), потом присваиваем этой переменной другую переменную (её возвращаемое значение);
+            console.log('Done!');
+        } else {
+            console.log('Error!');
+            i--; // Если инструкция if, выше, не выполнелась, мы обратимся к ней снова (внутри цикла) с помощью этого выражения;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) { // Один из способов (через логический оператор "и") задать диапазон;
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) { // Для того что бы задать, еще одно, альтернативное условие нужно использовать конструкцию else if;
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+// rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) { // Один из способов (через логический оператор "и") задать диапазон;
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) { // Для того что бы задать, еще одно, альтернативное условие нужно использовать конструкцию else if;
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i += 1) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); // Запись полученных от пользователя значений в массив;
+    }
+}
+
+writeYourGenres();
+
 
 console.log(personalMovieDB);
