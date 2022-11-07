d9e90fc14c09dc402245f266445915c0574701e5
Roman <hgpdfllh@gmail.com>
2022-11-07 11:48:00 +0200

    Change 04_functions.js, passed lesson 'callback'

--- js/04_functions.js
+++ js/04_functions.js
@@ -34,4 +34,30 @@ logger();
 const calc = (a, b) => {
     console.log('42');
     return a + b;
+};
+
+// Callback
+
+function first() {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
+
+function second() {
+    console.log(2);
+}
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок');
+}
+
+learnJS('JavaScript', done);
